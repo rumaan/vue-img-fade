9fc277cecae0c76144de08f7e04644f81e90507c
rumaan <rumaankalander@gmail.com>
2022-12-19 11:28:04 +0530

    add github docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, type BuildOptions } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "VueImgFade",
-      fileName: "vue-img-fade"
-    },
-    rollupOptions: {
-      external: ["vue"],
-
-      output: {
-        globals: {
-          vue: "Vue",
-        },
+const buildLibConfig: BuildOptions = {
+  lib: {
+    entry: resolve(__dirname, "src/index.ts"),
+    name: "VueImgFade",
+    fileName: "vue-img-fade",
+  },
+  rollupOptions: {
+    external: ["vue"],
+    output: {
+      globals: {
+        vue: "Vue",
       },
     },
   },
+};
+
+const buildDocsConfig: BuildOptions = {
+  outDir: "docs",
+};
+
+const buildConfig = process.env.BUNDLE ? buildLibConfig : buildDocsConfig;
+
+console.log("Building ->", process.env.BUNDLE ? "Library" : "Docs");
+
+export default defineConfig({
+  plugins: [vue()],
+  build: buildConfig,
 });
